8138d6fba0dbdffce3e8841d379bf78fe3dbb613
Ahsan Nirjhor <smahsanul0@gmail.com>
2023-08-14 21:46:14 +0600

    stack initial change

--- stack.c
+++ stack.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// define a stack using arrey and structure
+// define a stack using array and structure
 #define stackMax 100
 typedef struct {
     int top;
